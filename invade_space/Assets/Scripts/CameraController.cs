d654129fac3f88bea358a833d9abc231e7b0560b
OskarSzafer <68118272+OskarSzafer@users.noreply.github.com>
2024-08-03 21:16:23 +0200

    camera controller, scroll zoom

--- invade_space/Assets/Scripts/CameraController.cs
+++ invade_space/Assets/Scripts/CameraController.cs
@@ -8,8 +8,11 @@ public class CameraController : MonoBehaviour
     
     // Camera zoom
     private float elapsedTime;
+    private float zoomOut = 15.0f;
+    private float scrollZoomSpeed = 1.0f;
     [SerializeField] private float zoomIn = 6.0f;
-    [SerializeField] private float zoomOut = 10.0f;
+    [SerializeField] private float zoomOutMin = 10.0f;
+    [SerializeField] private float zoomOutMax = 20.0f;
     [SerializeField] private float zoomTime = 1.0f;
     [SerializeField] private AnimationCurve zoomCurve;
 
@@ -22,11 +25,12 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // following player
         if (player == null) return;
         
         transform.position = player.transform.position + new Vector3(0, 0, -10);
 
-        // Camera zoom
+        // zoom on planet
         elapsedTime = Mathf.Clamp(elapsedTime, 0.0f, zoomTime);
         float procetage = elapsedTime / zoomTime;
         GetComponent<Camera>().orthographicSize = Mathf.Lerp(zoomOut, zoomIn, zoomCurve.Evaluate(procetage));
@@ -38,6 +42,22 @@ public class CameraController : MonoBehaviour
         else
         {
             elapsedTime -= Time.deltaTime;
+            scrollZoom();
         }
+    }
+
+    // scroll zoom
+    void scrollZoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0.0f)
+        {
+            zoomOut -= scrollZoomSpeed;
+        }
+        else if (scroll < 0.0f)
+        {
+            zoomOut += scrollZoomSpeed;
+        }
+        zoomOut = Mathf.Clamp(zoomOut, zoomOutMin, zoomOutMax);
     }
 }
